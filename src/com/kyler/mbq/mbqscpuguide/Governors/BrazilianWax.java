3de09dc03663ba02b76fb80a0479ba547e768e43
MBQ <MBQsniper@hotmail.com>
2013-07-09 03:09:51 -0700

    minor stuff

--- src/com/kyler/mbq/mbqscpuguide/Governors/BrazilianWax.java
+++ src/com/kyler/mbq/mbqscpuguide/Governors/BrazilianWax.java
@@ -8,19 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.*;
+import android.widget.AdapterView.*;
 
-public class BrazilianWax extends Fragment implements OnClickListener {
+public class BrazilianWax extends Fragment implements OnClickListener
+	{
+
+		public void onClick(View p1)
+			{
+				// TODO: Implement this method
+			}
+		
+
 	
 	  @Override
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	      Bundle savedInstanceState) {
 	    final View v = inflater.inflate(R.layout.brazilianwaxlayout, container, false);
 	    return v;
-	  }
-
-	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+		}
 	}
