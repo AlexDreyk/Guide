6b863477ae00432569a0c6095dd44152b826f6ae
Kyler Jeffrey <mbqsniper@hotmail.com>
2013-07-08 19:10:54 -0700

    Margins for all

--- src/com/kyler/mbq/mbqscpuguide/Help.java
+++ src/com/kyler/mbq/mbqscpuguide/Help.java
@@ -5,17 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
 
-public class Help extends Fragment {
-
-ListView listview;
-
-
+public class Help extends Fragment {	
+	
   	  @Override
   	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
   	      Bundle savedInstanceState) {
   	    final View v = inflater.inflate(R.layout.helplayout, container, false);
   	    return v;
-  	  }
-  	}
+	}  	    	   
+}
--- src/com/kyler/mbq/mbqscpuguide/Schedulers/CFQ.java
+++ src/com/kyler/mbq/mbqscpuguide/Schedulers/CFQ.java
@@ -1,7 +1,6 @@
 package com.kyler.mbq.mbqscpuguide.Schedulers;
 
 import com.kyler.mbq.mbqscpuguide.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,10 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class CFQ extends Fragment {
+
 	  @Override
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	      Bundle savedInstanceState) {
 	    final View v = inflater.inflate(R.layout.cfqlayout, container, false);
 	    return v;
-	  }
 	}
+}
