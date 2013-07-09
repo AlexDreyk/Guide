8c39f4588243e2eb5308db41a79ccb2ee2607923
MBQ <MBQsniper@hotmail.com>
2013-07-08 17:04:58 -0700

    Make Help a ListView, no more shadow, add margins to the bfq layout (special thanks to dreamsforgotten)

--- src/com/kyler/mbq/mbqscpuguide/Help.java
+++ src/com/kyler/mbq/mbqscpuguide/Help.java
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 
 public class Help extends Fragment {
+
+ListView listview;
+
+
   	  @Override
   	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
   	      Bundle savedInstanceState) {
